1165aeab392e1a79495ab50af65c492ae133f6fc
Phea Soy <pheasoy@eBiz-admins-MacBook-Pro.local>
2019-08-15 20:50:11 +0700

    Add demo ELK

---
+++ src/main/java/org/soyphea/monitoring/MyLogFormat.java
@@ -0,0 +1,5 @@
+package org.soyphea.monitoring;
+
+public class MyLogFormat {
+
+}
--- src/main/java/org/soyphea/monitoring/SpringBootMonitoringApplication.java
+++ src/main/java/org/soyphea/monitoring/SpringBootMonitoringApplication.java
@@ -1,21 +1,75 @@
 package org.soyphea.monitoring;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.awt.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @RestController
-
 public class SpringBootMonitoringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMonitoringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMonitoringApplication.class, args);
+    }
 
-	@GetMapping("/greeting")
-	public String greeting(){
-		return "Hi from Spring Boot + Prometheus + Grafana";
-	}
+
+    @Autowired
+    BookService bookService;
+
+    @PostMapping("/book")
+    public ResponseEntity<?> greeting(@RequestBody Book book) {
+        Book resultBook = bookService.save(book);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(resultBook.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @Service
+    class BookService {
+
+        private java.util.List<Book> books = new ArrayList<>();
+
+        public Book save(Book book) {
+            Optional<Book> findBook = books.stream().filter(bk -> bk.getTitle().equals(book.getTitle())).findFirst();
+            if (findBook.isPresent()) {
+                throw new RuntimeException();
+            }
+            Book saveBook = bookService.save(book);
+            return saveBook;
+        }
+
+        public Book findBook(String title) {
+
+            return null;
+        }
+
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    class Book {
+        String id;
+        String title;
+        String author;
+
+    }
 }
